eb044cc7ef79de3fb423bff08c04958c771b99fa
Mansoor M. Sathir <sathir@live.com>
2021-01-11 09:06:54 +0530

    -> Write Compute Field and its Function

--- patient.py
+++ patient.py
@@ -8,6 +8,16 @@ from flectra import models, fields, api, _
 #     name = fields.Char()
 
 class HospitalPatient(models.Model):
+
+    @api.depends('patient_age')
+    def set_age_group(self):
+        for iteration in self:
+            if iteration.patient_age:
+                if iteration.patient_age < 18:
+                    iteration.age_group =  'minor'
+                else:
+                    iteration.age_group = 'major'
+
     _name = 'hospital.patient'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Patient Records'
@@ -19,6 +29,7 @@ class HospitalPatient(models.Model):
     image = fields.Binary(String='Image')
     name_seq = fields.Char(string='Patient Sequence', required=True, copy=False, readonly=True, index=True, default=lambda self: _('New'))
     gender = fields.Selection([('male','Male'),('fe_male','Female')], default='male', string="Gender")
+    age_group = fields.Selection([('major','Major'),('minor','Minor')], string="Age Group", compute='set_age_group')
 
     @api.model
     def create(self, vals):
