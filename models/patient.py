2959fdc591bc8f7d45cd19ece4d0af3ad378e8b3
Mansoor M. Sathir <sathir@live.com>
2021-01-25 15:03:44 +0530

    -> Smart Button (Appointment count as compute)

--- models/patient.py
+++ models/patient.py
@@ -30,6 +30,20 @@ class HospitalPatient(models.Model):
             if rec.patient_age <=5:
                 raise ValidationError(_('The Age must be Greater than 5'))
 
+    @api.multi
+    def open_patient_appointments(self):
+        return {
+            'name': _('Appointments'),
+            'domain': [('patient_id', '=', self.id)],
+            'view_type': 'form',
+            'res_model': 'hospital.appointment',
+            'view_id': False,
+            'view_mode': 'tree,form',
+            'type': 'ir.actions.act_window',
+        }
+    def get_appointment_count(self):
+        count = self.env['hospital.appointment'].search_count([('patient_id','=',self.id)])
+        self.appointment_count = count
 
     patient_name = fields.Char(String='Name', required=True)
     patient_age = fields.Integer('Age', track_visibility="always")
@@ -38,6 +52,7 @@ class HospitalPatient(models.Model):
     name_seq = fields.Char(string='Patient Sequence', required=True, copy=False, readonly=True, index=True, default=lambda self: _('New'))
     gender = fields.Selection([('male','Male'),('fe_male','Female')], default='male', string="Gender")
     age_group = fields.Selection([('major','Major'),('minor','Minor')], string="Age Group", compute='set_age_group')
+    appointment_count = fields.Integer(string='Appointment', compute='get_appointment_count')
 
     @api.model
     def create(self, vals):
